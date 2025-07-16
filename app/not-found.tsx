29b837c3e827cf3e0e73e9fa6b47d1674489b827
Karl Horky <karl.horky@gmail.com>
2025-07-16 11:49:42 +0200

    Add metadata exports to not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,9 @@
 import Link from 'next/link';
 
+export const metadata = {
+  title: 'Not found',
+};
+
 export default function RootNotFound() {
   // You can add any UI inside RootNotFound, including Animations.
   return (
